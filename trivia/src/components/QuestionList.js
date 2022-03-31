7180ea0f16dc0f7d44f0821516982c4558c16627
Paul Reece <paulreece2@gmail.com>
2022-03-31 11:01:01 -0400

    working

--- trivia/src/components/QuestionList.js
+++ trivia/src/components/QuestionList.js
@@ -5,6 +5,7 @@ import he from "he";
 
 const QuestionList = ({ id }) => {
   const [questions, setQuestions] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(0);
   useEffect(() => {
     axios
       .get(`https://opentdb.com/api.php?amount=10&category=${id}&type=multiple`)
@@ -13,22 +14,23 @@ const QuestionList = ({ id }) => {
   console.log(questions[0]?.type);
   return (
     <div>
-      {questions[0]?.question}
-      {questions[0]?.correct_answer}
-      {questions[0]?.incorrect_answers}
-      {questions.map((question, index) => (
-        <div>
-          <Question
-            key={index}
-            question={he.decode(question.question)}
-            correct_answer={he.decode(question.correct_answer)}
-            incorrect_answers={question.incorrect_answers.map(
-              (incorrect_answer) =>
-                (incorrect_answer = he.decode(incorrect_answer))
-            )}
-          />
-        </div>
-      ))}
+      {questions.map((question, index) => {
+        if (index === questionIndex) {
+          return (
+            <div>
+              <Question
+                key={index}
+                question={he.decode(question.question)}
+                correct_answer={he.decode(question.correct_answer)}
+                incorrect_answers={question.incorrect_answers.map(
+                  (incorrect_answer) =>
+                    (incorrect_answer = he.decode(incorrect_answer))
+                )}
+              />
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
